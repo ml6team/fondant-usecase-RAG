bb5cb2ce452916a454c72aa1ec06c3e29e4774d6
Hakimovich99 <hakim.amri@ulb.be>
2023-11-21 16:40:06 +0100

    retrieve from weaviate using weaviate instances

--- src/components/retrieve_from_weaviate/src/main.py
+++ src/components/retrieve_from_weaviate/src/main.py
@@ -1,25 +1,18 @@
 import dask
 import pandas as pd
 from fondant.component import PandasTransformComponent
-from llama_index import ServiceContext, VectorStoreIndex
-from llama_index.embeddings import HuggingFaceEmbedding
-from llama_index.vector_stores import WeaviateVectorStore
 
 import weaviate
-
-dask.config.set({"dataframe.convert-string": False})
 
 
 class RetrieveChunks(PandasTransformComponent):
     def __init__(
-        self,
-        *_,
-        weaviate_url: str,
-        class_name: str,
-        text_property_name: str,
-        hf_embed_model: str,
-        top_k: int
-    ) -> None:
+            self,
+            *_,
+            weaviate_url: str,
+            class_name: str,
+            top_k: int
+        ) -> None:
         """
         Args:
             weaviate_url: An argument passed to the component
@@ -27,29 +20,26 @@ class RetrieveChunks(PandasTransformComponent):
         # Initialize your component here based on the arguments
         self.client = weaviate.Client(weaviate_url)
         self.class_name = class_name
-        self.text_property_name = text_property_name
-        self.model = HuggingFaceEmbedding(hf_embed_model)
         self.k = top_k
-        self.retriever = self._set_retriever(
-            self.client, self.class_name, self.model, self.k
+    
+    def retrieve_chunks(self, vector_query: str):
+        """Get results from weaviate database"""
+    
+        result = (
+        self.client.query
+        .get(self.class_name, ["passage"])
+        .with_near_vector({"vector":vector_query})
+        .with_limit(self.k)
+        .with_additional(["distance"])
+        .do()
         )
+        result_dict = result["data"]["Get"][self.class_name]
+        text = [retrieved_chunk["passage"] for retrieved_chunk in result_dict]
 
-    def _set_retriever(self, client, class_name, model, k):
-        vector_store = WeaviateVectorStore(
-            weaviate_client=client, index_name=class_name
-        )
-        service_context = ServiceContext.from_defaults(llm=None, embed_model=model)
-        indexed_vector_db = VectorStoreIndex.from_vector_store(
-            vector_store=vector_store, service_context=service_context
-        )
-        return indexed_vector_db.as_retriever(similarity_top_k=k)
-
-    def retrieve_chunks(self, query: str):
-        retrievals = self.retriever.retrieve(query)
-        return [chunk.metadata[self.text_property_name] for chunk in retrievals]
+        return text
 
     def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        dataframe[("text", "retrieved_chunks")] = dataframe[("text", "question")].apply(
+        dataframe[("text", "retrieved_chunks")] = dataframe[("text", "embedding")].apply(
             self.retrieve_chunks
         )
         return dataframe
