0b776acaf222c5d5ee6f55a68ed6e0309288b7de
Hakimovich99 <hakim.amri@ulb.be>
2023-11-22 09:38:04 +0100

    pre-commit fix

--- src/components/retrieve_from_weaviate/src/main.py
+++ src/components/retrieve_from_weaviate/src/main.py
@@ -1,4 +1,3 @@
-import dask
 import pandas as pd
 from fondant.component import PandasTransformComponent
 
@@ -6,13 +5,7 @@ import weaviate
 
 
 class RetrieveChunks(PandasTransformComponent):
-    def __init__(
-            self,
-            *_,
-            weaviate_url: str,
-            class_name: str,
-            top_k: int
-        ) -> None:
+    def __init__(self, *_, weaviate_url: str, class_name: str, top_k: int) -> None:
         """
         Args:
             weaviate_url: An argument passed to the component
@@ -21,17 +14,16 @@ class RetrieveChunks(PandasTransformComponent):
         self.client = weaviate.Client(weaviate_url)
         self.class_name = class_name
         self.k = top_k
-    
+
     def retrieve_chunks(self, vector_query: str):
         """Get results from weaviate database"""
-    
+
         result = (
-        self.client.query
-        .get(self.class_name, ["passage"])
-        .with_near_vector({"vector":vector_query})
-        .with_limit(self.k)
-        .with_additional(["distance"])
-        .do()
+            self.client.query.get(self.class_name, ["passage"])
+            .with_near_vector({"vector": vector_query})
+            .with_limit(self.k)
+            .with_additional(["distance"])
+            .do()
         )
         result_dict = result["data"]["Get"][self.class_name]
         text = [retrieved_chunk["passage"] for retrieved_chunk in result_dict]
@@ -39,7 +31,7 @@ class RetrieveChunks(PandasTransformComponent):
         return text
 
     def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        dataframe[("text", "retrieved_chunks")] = dataframe[("text", "embedding")].apply(
-            self.retrieve_chunks
-        )
+        dataframe[("text", "retrieved_chunks")] = dataframe[
+            ("text", "embedding")
+        ].apply(self.retrieve_chunks)
         return dataframe
