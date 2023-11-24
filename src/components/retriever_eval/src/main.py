b9c03a2327c031f07b790bef21cc0fb51db4d1d8
Hakimovich99 <hakim.amri@ulb.be>
2023-11-24 16:10:28 +0100

    last fixes
    - added argument to choose RAGAS metrics to compute. Yet, only context_precision and context_relevancy can be used because they are the only ones requiring "question" and "retrieved context". Other metrics needs either answer generated or ground truth answer, meaning that the input Dataset must differ. --> suggest to keep those for later.
    - retrieve workaround for empty parquet files.

--- src/components/retriever_eval/src/main.py
+++ src/components/retriever_eval/src/main.py
@@ -4,20 +4,37 @@ from fondant.component import PandasTransformComponent
 from langchain.llms import OpenAI
 from ragas import evaluate
 from ragas.llms import LangchainLLM
-from ragas.metrics import context_precision, context_relevancy
 
 
 class RetrieverEval(PandasTransformComponent):
-    def __init__(self, *_, openai_key) -> None:
+    def __init__(self, *_, openai_key, metrics) -> None:
         """
         Args:
             openai_key: OpenAI key
+            metrics: RAGAS metrics to compute
         """
         llm = OpenAI(openai_api_key=openai_key)
-        gpt_wrapper = LangchainLLM(llm=llm)
-        context_precision.llm = gpt_wrapper
-        context_relevancy.llm = gpt_wrapper
+        self.gpt_wrapper = LangchainLLM(llm=llm)
+        self.metric_functions = self.extract_metric_functions(metrics=metrics)
+        self.set_llm(self.metric_functions)
 
+    #import the metric functions selected
+    @staticmethod
+    def import_from(module, name):
+        module = __import__(module, fromlist=[name])
+        return getattr(module, name)
+    
+    def extract_metric_functions(self, metrics: list):
+        functions = []
+        for metric in metrics:
+            functions.append(self.import_from("ragas.metrics", metric))
+        return functions
+    
+    def set_llm(self, metric_functions: list):
+        for metric_function in metric_functions:
+            metric_function.llm = self.gpt_wrapper
+
+    #evaluate the retriever
     @staticmethod
     def create_hf_ds(dataframe: pd.DataFrame):
         dataframe.rename(
@@ -25,10 +42,9 @@ class RetrieverEval(PandasTransformComponent):
         )
         return Dataset.from_pandas(dataframe)
 
-    @staticmethod
-    def ragas_eval(dataset):
+    def ragas_eval(self, dataset):
         result = evaluate(
-            dataset=dataset, metrics=[context_precision, context_relevancy]
+            dataset=dataset, metrics=self.metric_functions
         )
         return result
 
@@ -51,17 +67,3 @@ class RetrieverEval(PandasTransformComponent):
             )
 
             return results_df
-
-        # in case empty parquet files, create dummy dataframe
-        else:
-            empty = pd.DataFrame(
-                {
-                    "question": "",
-                    "contexts": [" "],
-                    "context+precision": 0.01,
-                    "context+relevancy": 0.01,
-                }
-            )
-            empty.index = empty.index.astype(str)
-            empty.columns = pd.MultiIndex.from_product([["text"], empty.columns])
-            return empty
