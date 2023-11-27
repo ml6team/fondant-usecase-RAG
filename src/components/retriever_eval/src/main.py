76f0102afc23ea2e45a87b3058f4c531af7361ee
Hakimovich99 <hakim.amri@ulb.be>
2023-11-27 09:43:09 +0100

    Update main.py

--- src/components/retriever_eval/src/main.py
+++ src/components/retriever_eval/src/main.py
@@ -52,16 +52,15 @@ class RetrieverEval(PandasTransformComponent):
         )
         if "id" in hf_dataset.column_names:
             hf_dataset = hf_dataset.remove_columns("id")
-        print(hf_dataset)
-        if hf_dataset.num_rows != 0:
-            result = self.ragas_eval(dataset=hf_dataset)
-            results_df = result.to_pandas()
-            results_df.index = results_df.index.astype(str)
-            # rename columns to avoid issues with following component
-            results_df.columns = results_df.columns.str.replace("_", "+")
-            # Set multi-index column for the expected subset and field
-            results_df.columns = pd.MultiIndex.from_product(
-                [["text"], results_df.columns],
-            )
 
-            return results_df
+        result = self.ragas_eval(dataset=hf_dataset)
+        results_df = result.to_pandas()
+        results_df.index = results_df.index.astype(str)
+        # rename columns to avoid issues with following component
+        results_df.columns = results_df.columns.str.replace("_", "+")
+        # Set multi-index column for the expected subset and field
+        results_df.columns = pd.MultiIndex.from_product(
+            [["text"], results_df.columns],
+        )
+
+        return results_df
