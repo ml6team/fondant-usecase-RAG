6817291f47a532b8ad873806cf48f84b277d6c30
Hakimovich99 <hakim.amri@ulb.be>
2023-11-28 14:16:54 +0100

    retrieve metrics argument
    Actually not interesting to have this argument. The user will already choose the metrics to compute in the "retriever_eval" component, no need to choose again which metrics to agg, we can just agg them all.

--- src/components/aggregate_eval_results/src/main.py
+++ src/components/aggregate_eval_results/src/main.py
@@ -3,23 +3,13 @@ from fondant.component import DaskTransformComponent
 
 
 class AggregateResults(DaskTransformComponent):
-    def __init__(self, *_, metrics: list) -> None:
-        """
-        Args:
-            metrics: Metrics chosen to be aggregated
-        """
-
-        self.metrics = metrics
+    def __init__(self, *_) -> None:
+        return None
 
     def transform(self, dataframe: dd.DataFrame) -> dd.DataFrame:
-        if self.metrics is None:
-            chosen_metrics = list(
+        metrics = list(
                 dataframe["text"].select_dtypes(["float", "int"]).columns
             )
-        else:
-            chosen_metrics = self.metrics
-        metrics = [f"text_{metric}" for metric in chosen_metrics]
-
         agg = dataframe[metrics].mean()
         agg_df = agg.to_frame(name="score")
         agg_df["metric"] = agg.index
