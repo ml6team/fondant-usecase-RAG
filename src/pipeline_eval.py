6334a077bf8ce6a1718bbbac8868f5181a524602
hakiamri <117269156+Hakimovich99@users.noreply.github.com>
2023-11-30 21:45:30 +0100

    example pipeline (#28)
    - questions dataset: 40 questions generated from 1000 first rows of
    wikitext (same as the one loaded in pipeline.py)
    - uses the same embed model as in pipeline.py 
    --> whole pipeline example: run pipeline.py (with n_rows_to_load = 1000
    in load component) to index the weaviate db, then run pipeline_eval.py
    to get at the end the aggregated results

---
+++ src/pipeline_eval.py
@@ -0,0 +1,60 @@
+"""Pipeline used to evaluate a RAG pipeline."""
+import logging
+
+from fondant.pipeline import ComponentOp, Pipeline
+
+logger = logging.getLogger(__name__)
+
+
+pipeline = Pipeline(
+    pipeline_name="evaluation-pipeline",
+    pipeline_description="Pipeline to evaluate \
+    a RAG solution",
+    base_path="./data-dir",  # The demo pipelines uses a local \
+    # directory to store the data.
+)
+
+load_from_csv = ComponentOp(
+    component_dir="components/load_from_csv",
+    arguments={
+        # Add arguments
+        "dataset_uri": "/data/wikitext_1000_q.csv",
+        "column_separator": ";",
+        "column_name_mapping": {"question": "text_data"},
+    },
+)
+
+embed_text_op = ComponentOp.from_registry(
+    name="embed_text",
+    arguments={
+        "model_provider": "huggingface",
+        "model": "all-MiniLM-L6-v2",
+    },
+)
+
+retrieve_chunks = ComponentOp(
+    component_dir="components/retrieve_from_weaviate",
+    arguments={
+        "weaviate_url": "http://host.docker.internal:8080",
+        "class_name": "Index",
+        "top_k": 2,
+    },
+)
+
+retriever_eval = ComponentOp(
+    component_dir="components/retriever_eval",
+    arguments={
+        "llm_name": "OpenAI",
+        "llm_kwargs": {"openai_api_key": ""},
+        "metrics": ["context_precision", "context_relevancy"],
+    },
+)
+
+aggregate_results = ComponentOp(component_dir="components/aggregate_eval_results")
+
+# Construct your pipeline
+pipeline.add_op(load_from_csv)
+pipeline.add_op(embed_text_op, dependencies=load_from_csv)
+pipeline.add_op(retrieve_chunks, dependencies=embed_text_op)
+pipeline.add_op(retriever_eval, dependencies=retrieve_chunks)
+pipeline.add_op(aggregate_results, dependencies=retriever_eval)
