ffbae3d2cbab0b4027fe568f2d9ecc1a36e0c2b6
Hakimovich99 <hakim.amri@ulb.be>
2023-12-04 10:38:19 +0100

    Update pipeline_eval.py
    not definitive, still component_dir to tackle/adapt

--- src/pipeline_eval.py
+++ src/pipeline_eval.py
@@ -15,12 +15,12 @@ pipeline = Pipeline(
 )
 
 load_from_csv = ComponentOp(
-    component_dir="components/load_from_csv",
+    component_dir="/Users/hakimamri/Documents/GitHub/fondant-usecase-RAG/src/components/load_from_csv",
     arguments={
         # Add arguments
         "dataset_uri": "/data/wikitext_1000_q.csv",
         "column_separator": ";",
-        "column_name_mapping": {"question": "text_data"},
+        "column_name_mapping": {"question": "text"},
     },
 )
 
@@ -33,7 +33,7 @@ embed_text_op = ComponentOp.from_registry(
 )
 
 retrieve_chunks = ComponentOp(
-    component_dir="components/retrieve_from_weaviate",
+    component_dir="/Users/hakimamri/Documents/GitHub/fondant-usecase-RAG/src/components/retrieve_from_weaviate",
     arguments={
         "weaviate_url": "http://host.docker.internal:8080",
         "class_name": "Index",
@@ -42,7 +42,7 @@ retrieve_chunks = ComponentOp(
 )
 
 retriever_eval = ComponentOp(
-    component_dir="components/retriever_eval",
+    component_dir="/Users/hakimamri/Documents/GitHub/fondant-usecase-RAG/src/components/retriever_eval",
     arguments={
         "llm_name": "OpenAI",
         "llm_kwargs": {"openai_api_key": ""},
@@ -50,7 +50,9 @@ retriever_eval = ComponentOp(
     },
 )
 
-aggregate_results = ComponentOp(component_dir="components/aggregate_eval_results")
+aggregate_results = ComponentOp(
+    component_dir="/Users/hakimamri/Documents/GitHub/fondant-usecase-RAG/src/components/aggregate_eval_results",
+)
 
 # Construct your pipeline
 pipeline.add_op(load_from_csv)
