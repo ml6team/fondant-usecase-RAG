d18b79179a35a66909c9d7bea5201ab6a91461c9
Hakimovich99 <hakim.amri@ulb.be>
2023-12-05 17:52:33 +0100

    evaluation notebook
    - First iteration works well
    - For second iteration with other parameters, get a "cannot read parquet file" error for evaluation pipeline. to investigate
    - weaviate_url in component: must put IP address (host.docker or localhost url don't work). to investigate
    - Cannot run script with Fondant CLI, doesn't find the pipeline. to investigate

--- src/pipeline_eval.py
+++ src/pipeline_eval.py
@@ -5,56 +5,90 @@ from fondant.pipeline import ComponentOp, Pipeline
 
 logger = logging.getLogger(__name__)
 
+def create_pipeline(
+        #fixed args
+        pipeline_dir: str,
+        embed_model_provider: str,
+        embed_model: str,
+        weaviate_url: str,
+        weaviate_class_name: str,
+        #custom args
+        csv_dataset_uri: str,
+        csv_column_separator: str,
+        question_column_name: str,
+        top_k: int,
+        llm_name: str,
+        llm_kwargs: dict,
+        metrics: list,
+):
 
-pipeline = Pipeline(
-    pipeline_name="evaluation-pipeline",
-    pipeline_description="Pipeline to evaluate \
-    a RAG solution",
-    base_path="./data-dir",  # The demo pipelines uses a local \
-    # directory to store the data.
-)
+    evaluation_pipeline = Pipeline(
+        pipeline_name="evaluation-pipeline",
+        pipeline_description="Pipeline to evaluate \
+        a RAG solution",
+        base_path=pipeline_dir,  # The demo pipelines uses a local \
+        # directory to store the data.
+    )
 
-load_from_csv = ComponentOp(
-    component_dir="components/load_from_csv",
-    arguments={
-        # Add arguments
-        "dataset_uri": "/data/wikitext_1000_q.csv",
-        "column_separator": ";",
-        "column_name_mapping": {"question": "text"},
-    },
-)
+    load_from_csv = ComponentOp(
+        component_dir="components/load_from_csv",
+        arguments={
+            # Add arguments
+            "dataset_uri": csv_dataset_uri,
+            "column_separator": csv_column_separator,
+            "column_name_mapping": {question_column_name: "text"},
+        },
+    )
 
-embed_text_op = ComponentOp.from_registry(
-    name="embed_text",
-    arguments={
-        "model_provider": "huggingface",
-        "model": "all-MiniLM-L6-v2",
-    },
-)
+    embed_text_op = ComponentOp.from_registry(
+        name="embed_text",
+        arguments={
+            "model_provider": embed_model_provider,
+            "model": embed_model,
+        },
+    )
 
-retrieve_chunks = ComponentOp(
-    component_dir="components/retrieve_from_weaviate",
-    arguments={
-        "weaviate_url": "http://host.docker.internal:8080",
-        "class_name": "Index",
-        "top_k": 2,
-    },
-)
+    retrieve_chunks = ComponentOp(
+        component_dir="components/retrieve_from_weaviate",
+        arguments={
+            "weaviate_url": weaviate_url,
+            "class_name": weaviate_class_name,
+            "top_k": top_k,
+        },
+    )
 
-retriever_eval = ComponentOp(
-    component_dir="components/retriever_eval",
-    arguments={
-        "llm_name": "OpenAI",
-        "llm_kwargs": {"openai_api_key": ""},
-        "metrics": ["context_precision", "context_relevancy"],
-    },
-)
+    retriever_eval = ComponentOp(
+        component_dir="components/retriever_eval",
+        arguments={
+            "llm_name": llm_name,
+            "llm_kwargs": llm_kwargs,
+            "metrics": metrics,
+        },
+    )
 
-aggregate_results = ComponentOp(component_dir="components/aggregate_eval_results")
+    aggregate_results = ComponentOp(component_dir="components/aggregate_eval_results")
 
-# Construct your pipeline
-pipeline.add_op(load_from_csv)
-pipeline.add_op(embed_text_op, dependencies=load_from_csv)
-pipeline.add_op(retrieve_chunks, dependencies=embed_text_op)
-pipeline.add_op(retriever_eval, dependencies=retrieve_chunks)
-pipeline.add_op(aggregate_results, dependencies=retriever_eval)
+    # Construct your pipeline
+    evaluation_pipeline.add_op(load_from_csv)
+    evaluation_pipeline.add_op(embed_text_op, dependencies=load_from_csv)
+    evaluation_pipeline.add_op(retrieve_chunks, dependencies=embed_text_op)
+    evaluation_pipeline.add_op(retriever_eval, dependencies=retrieve_chunks)
+    evaluation_pipeline.add_op(aggregate_results, dependencies=retriever_eval)
+
+    return evaluation_pipeline
+
+if __name__ == "__main__":
+    pipeline = create_pipeline(
+        pipeline_dir="./data-dir",
+        embed_model_provider="huggingface",
+        embed_model="all-MiniLM-L6-v2",
+        weaviate_url="http://host.docker.internal:8080",
+        weaviate_class_name="Pipeline_1",
+        csv_dataset_uri="/data/wikitext_1000_q.csv", #make sure it is the same as mounted file
+        csv_column_separator=";",
+        question_column_name="question",
+        top_k=3,
+        llm_name="OpenAI",
+        llm_kwargs={"openai_api_key": ""},
+        metrics=["context_precision", "context_relevancy"]
+    )
---
+++ src/pipeline_index.py
@@ -0,0 +1,85 @@
+"""Pipeline used to create a stable diffusion dataset from a set of given images."""
+import logging
+
+from fondant.pipeline import ComponentOp, Pipeline
+
+logger = logging.getLogger(__name__)
+
+def create_pipeline(
+        #fixed args
+        pipeline_dir: str,
+        embed_model_provider: str,
+        embed_model: str,
+        weaviate_url: str,
+        weaviate_class_name: str,
+        #custom args
+        hf_dataset_name: str,
+        data_column_name: str,
+        n_rows_to_load: int,
+        chunk_size: int,
+        chunk_overlap: int,
+):
+
+    indexing_pipeline = Pipeline(
+        pipeline_name="ingestion-pipeline",
+        pipeline_description="Pipeline to prepare and process \
+        data for building a RAG solution",
+        base_path=pipeline_dir,  # The demo pipelines uses a local \
+        # directory to store the data.
+    )
+
+    load_from_hf_hub = ComponentOp(
+        component_dir="components/load_from_hf_hub",
+        arguments={
+            # Add arguments
+            "dataset_name": hf_dataset_name,
+            "column_name_mapping": {data_column_name: "text"},
+            "n_rows_to_load": n_rows_to_load,
+        },
+    )
+
+    chunk_text_op = ComponentOp.from_registry(
+        name="chunk_text",
+        arguments={
+            "chunk_size": chunk_size,
+            "chunk_overlap": chunk_overlap,
+        },
+    )
+
+    embed_text_op = ComponentOp.from_registry(
+        name="embed_text",
+        arguments={
+            "model_provider": embed_model_provider,
+            "model": embed_model,
+        },
+    )
+
+    index_weaviate_op = ComponentOp.from_registry(
+        name="index_weaviate",
+        arguments={
+            "weaviate_url": weaviate_url,
+            "class_name": weaviate_class_name,
+        },
+    )
+
+    # Construct your pipeline
+    indexing_pipeline.add_op(load_from_hf_hub)
+    indexing_pipeline.add_op(chunk_text_op, dependencies=load_from_hf_hub)
+    indexing_pipeline.add_op(embed_text_op, dependencies=chunk_text_op)
+    indexing_pipeline.add_op(index_weaviate_op, dependencies=embed_text_op)
+
+    return indexing_pipeline
+
+if __name__ == "__main__":
+    pipeline = create_pipeline(
+        pipeline_dir="./data-dir",
+        embed_model_provider="huggingface",
+        embed_model="all-MiniLM-L6-v2",
+        weaviate_url="http://host.docker.internal:8080",
+        weaviate_class_name="Pipeline_1",
+        hf_dataset_name="wikitext@~parquet",
+        data_column_name="text",
+        n_rows_to_load=1000,
+        chunk_size=512,
+        chunk_overlap=32
+    )
