cc1bb9d5b52b9aaf4f804e7ad8d6a1f45c2ac3f7
Robbe Sneyders <robbe.sneyders@ml6.eu>
2023-12-07 10:47:03 +0100

    Update index pipeline and notebook for 0.8.dev4

--- src/components/text_cleaning/src/main.py
+++ src/components/text_cleaning/src/main.py
@@ -1,13 +1,9 @@
-import logging
-
 import pandas as pd
 from fondant.component import PandasTransformComponent
 
-logger = logging.getLogger(__name__)
-
 
 class TextCleaningComponent(PandasTransformComponent):
-    def __init__(self, *_):
+    def __init__(self, **kwargs):
         """Initialize your component."""
 
     def remove_empty_lines(self, text):
@@ -16,7 +12,7 @@ class TextCleaningComponent(PandasTransformComponent):
         return "\n".join(non_empty_lines)
 
     def transform(self, dataframe: pd.DataFrame) -> pd.DataFrame:
-        dataframe[("text", "data")] = dataframe[("text", "data")].apply(
-            lambda x: self.remove_empty_lines,
+        dataframe["text"] = dataframe["text"].apply(
+            self.remove_empty_lines,
         )
         return dataframe
