649eef04b6b6703deaaad14a96cfb0476871dcb6
Matthias Richter <matthias.r1092@gmail.com>
2024-02-06 15:35:09 +0100

    update eval pipeline

--- src/pipeline_eval.py
+++ src/pipeline_eval.py
@@ -2,6 +2,9 @@
 
 import pyarrow as pa
 from fondant.pipeline import Pipeline, Resources
+from components.retrieve_from_weaviate import RetrieveFromWeaviateComponent
+from components.evaluate_ragas import RagasEvaluator
+from components.aggregrate_eval_results import AggregateResults
 
 
 def create_pipeline(
@@ -10,7 +13,6 @@ def create_pipeline(
     weaviate_url="http://host.docker.internal:8080",
     weaviate_class: str = "Pipeline1",
     evaluation_set_path="./evaluation_datasets",
-    evaluation_set_filename="wikitext_1000_q.csv",
     evaluation_set_separator: str = ";",
     embed_model_provider: str = "huggingface",
     embed_model: str = "all-MiniLM-L6-v2",
@@ -19,7 +21,6 @@ def create_pipeline(
     llm_module_name: str = "langchain.chat_models",
     llm_class_name: str = "ChatOpenAI",
     llm_kwargs: dict = {"model_name": "gpt-3.5-turbo"},
-    evaluation_metrics: list = ["context_precision", "context_relevancy"],
     number_of_accelerators=None,
     accelerator_name=None,
 ):
@@ -33,7 +34,7 @@ def create_pipeline(
     load_from_csv = evaluation_pipeline.read(
         "load_from_csv",
         arguments={
-            "dataset_uri": "/evaldata/" + evaluation_set_filename,
+            "dataset_uri": evaluation_set_path,
             # mounted dir from within docker as extra_volumes
             "column_separator": evaluation_set_separator,
         },
@@ -60,28 +61,29 @@ def create_pipeline(
     )
 
     retrieve_chunks = embed_text_op.apply(
-        "retrieve_from_weaviate",
-        arguments={
-            "weaviate_url": weaviate_url,
-            "class_name": weaviate_class,
-            "top_k": retrieval_top_k,
-        },
-        cache=False,
+    RetrieveFromWeaviateComponent,
+    arguments={
+        "weaviate_url": weaviate_url,
+        "class_name": weaviate_class,
+        "top_k": retrieval_top_k
+    },
     )
 
     retriever_eval = retrieve_chunks.apply(
-        "evaluate_ragas",
+        RagasEvaluator,
         arguments={
             "llm_module_name": llm_module_name,
             "llm_class_name": llm_class_name,
-            "llm_kwargs": llm_kwargs,
-        },
-        produces={metric: pa.float32() for metric in evaluation_metrics},
+            "llm_kwargs": llm_kwargs
+        }
     )
 
     retriever_eval.apply(
-        "components/aggregate_eval_results",
-        consumes={metric: pa.float32() for metric in evaluation_metrics},
+        AggregateResults, 
+        consumes={
+            "context_precision": "context_precision",
+            "context_relevancy": "context_relevancy"
+        }
     )
 
     return evaluation_pipeline
